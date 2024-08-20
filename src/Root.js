aee4985fa3ade2572ae7d02624d235afc3db3c99
taejin.ahn <xowls0460@gmail.com>
2024-08-21 00:14:20 +0900

    squat challenge 기본 세팅

--- src/Root.js
+++ src/Root.js
@@ -1,9 +1,17 @@
+import { HashRouter } from "react-router-dom";
 import App from "./App";
+import { RecoilRoot } from "recoil";
 
 function Root() {
-  return <>
-    <App/>
-  </>
+  return (
+    <>
+      <HashRouter>
+        <RecoilRoot>
+          <App />
+        </RecoilRoot>
+      </HashRouter>
+    </>
+  );
 }
 
 export default Root;
